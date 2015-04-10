a2ca4ff461b007b06480ac4302bf6391cf18f364
rccoder <rccoder.net@gmail.com>
2015-04-10 19:47:59 +0800

    规划路由

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,25 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/reg', function(req, res, next) {
+	res.render('reg', { title: 'Reg'});
+});
+router.post('/reg', function(req, res) {
 
+});
+router.get('/login', function(req, res, next){
+	res.render('login', { title: 'Login'});
+});
+router.post('/login', function(req, res) {
+
+});
+router.get('/post', function(req, res, next) {
+	res.render('/post', { title: 'Post'});
+});
+router.post('/post', function(req, res) {
+
+});
+router.get('/logout', function(req, res, next) {
+
+});
 module.exports = router;

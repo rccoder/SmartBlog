ccc33843e6ef7608c81de106fe97adc6c1473e2f
skyline75489 <skyline75489@outlook.com>
2015-04-14 19:18:02 +0800

    使用两个空格的 Soft Tab 缩进

--- models/post.js
+++ models/post.js
@@ -2,130 +2,130 @@ var mongodb = require('./db');
 var markdown = require('markdown').markdown;
 
 function Post(name, title, post) {
-	this.name = name,
-	this.title = title,
-	this.post = post
+  this.name = name,
+  this.title = title,
+  this.post = post
 }
 
 module.exports = Post;
 
 Post.prototype.save = function(callback) {
-	var date = new Date();
-	var time = {
-		date: date,
-		year: date.getFullYear(),
-		month: date.getFullYear() + '-' + (date.getMonth() + 1),
-		day: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
-		minute: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
-			date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
-	};
-	var post = {
-		name: this.name,
-		time: time,
-		title: this.title,
-		post: this.post
-	};
-	console.log(post.time);
-	mongodb.open(function(err, db) {
-		if (err) {
-			return callback(err);
-		}
-		db.collection('posts', function(err, collection) {
-			if (err) {
-				mongodb.close();
-				return callback(err);
-			}
-			collection.insert(post, {
-				safe: true
-			}, function(err) {
-				mongodb.close();
-				if (err) {
-					return callback(err);
-				}
-				callback(null);
-			});
-		});
-	});
+  var date = new Date();
+  var time = {
+    date: date,
+    year: date.getFullYear(),
+    month: date.getFullYear() + '-' + (date.getMonth() + 1),
+    day: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
+    minute: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+      date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
+  };
+  var post = {
+    name: this.name,
+    time: time,
+    title: this.title,
+    post: this.post
+  };
+  console.log(post.time);
+  mongodb.open(function(err, db) {
+    if (err) {
+      return callback(err);
+    }
+    db.collection('posts', function(err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      collection.insert(post, {
+        safe: true
+      }, function(err) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null);
+      });
+    });
+  });
 };
 
 Post.get = function(name, callback) {
-	mongodb.open(function(err, db) {
-		if (err) {
-			return callback(err);
-		}
-		db.collection('posts', function(err, collection) {
-			if (err) {
-				mongodb.close();
-				return callback(err);
-			}
-			var query = {};
-			if (name) {
-				query.name = name;
-			}
-			collection.find(query).sort({
-				time: -1
-			}).toArray(function(err, docs) {
-				mongodb.close();
-				if (err) {
-					return callback(err);
-				}
-				docs.forEach(function(doc) {
-					doc.post = markdown.toHTML(doc.post);
-				});
-				callback(null, docs);
-			});
-		});
-	});
+  mongodb.open(function(err, db) {
+    if (err) {
+      return callback(err);
+    }
+    db.collection('posts', function(err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      var query = {};
+      if (name) {
+        query.name = name;
+      }
+      collection.find(query).sort({
+        time: -1
+      }).toArray(function(err, docs) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        docs.forEach(function(doc) {
+          doc.post = markdown.toHTML(doc.post);
+        });
+        callback(null, docs);
+      });
+    });
+  });
 };
 
 Post.getOne = function(name, day, title, callback) {
-	mongodb.open(function(err, db) {
-		if(err) {
-			mongodb.close();
-			return callback(err);
-		}
-		db.collection('posts', function(err, collection) {
-			if(err) {
-				mongodb.close();
-				return callback(err);
-			}
-			collection.findOne({
-				"name": name,
-				"time.day": day,
-				"title": title
-			}, function(err, doc) {
-				mongodb.close();
-				if(err) {
-					return callback(err);
-				}
-				doc.post = markdown.toHTML(doc.post);
-				callback(null, doc);
-			});
-		});
-	});
+  mongodb.open(function(err, db) {
+    if(err) {
+      mongodb.close();
+      return callback(err);
+    }
+    db.collection('posts', function(err, collection) {
+      if(err) {
+        mongodb.close();
+        return callback(err);
+      }
+      collection.findOne({
+        "name": name,
+        "time.day": day,
+        "title": title
+      }, function(err, doc) {
+        mongodb.close();
+        if(err) {
+          return callback(err);
+        }
+        doc.post = markdown.toHTML(doc.post);
+        callback(null, doc);
+      });
+    });
+  });
 };
 Post.edit = function(name, day, title, callback) {
-	mongodb.open(function(err, db) {
-		if(err) {
-			mongodb.close();
-			return callback(err);
-		}
-		db.collection('posts', function(err, collection) {
-			if(err) {
-				mongodb.close();
-				return callback(err);
-			}
-			collection.findOne({
-				"name": name,
-				"time.day": day,
-				"title": title
-			}, function(err, doc) {
-				mongodb.close();
-				if(err) {
-					return callback(err);
-				}
-				callback(null, doc);
-			});
-		});
-	});
+  mongodb.open(function(err, db) {
+    if(err) {
+      mongodb.close();
+      return callback(err);
+    }
+    db.collection('posts', function(err, collection) {
+      if(err) {
+        mongodb.close();
+        return callback(err);
+      }
+      collection.findOne({
+        "name": name,
+        "time.day": day,
+        "title": title
+      }, function(err, doc) {
+        mongodb.close();
+        if(err) {
+          return callback(err);
+        }
+        callback(null, doc);
+      });
+    });
+  });
 };
--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,14 @@ router.use(function(req, res, next) {
       req.flash('error', '未登录');
       res.redirect('/login');
     } else {
-    	next();
+      next();
     }
   } else if (skipIfLoggedIn.indexOf(req.path) > -1) {
     if (req.session.user) {
       req.flash('error', '已经登录');
       res.redirect('back');
     } else {
-    	next();
+      next();
     }
   } else {
     next();
@@ -29,197 +29,197 @@ router.use(function(req, res, next) {
 
 // Home
 router.get('/', function(req, res, next) {
-	Post.get(null, function(err, posts) {
-		if (err) {
-			posts = [];
-		}
-		res.render('index', {
-			title: 'Home',
-			user: req.session.user,
-			posts: posts,
-			success: req.flash('success').toString(),
-			error: req.flash('error').toString()
-		});
-	});
+  Post.get(null, function(err, posts) {
+    if (err) {
+      posts = [];
+    }
+    res.render('index', {
+      title: 'Home',
+      user: req.session.user,
+      posts: posts,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
+  });
 });
 
 // Register
 router.get('/reg', function(req, res, next) {
-	res.render('reg', {
-		title: 'Reg',
-		user: req.session.user,
-		success: req.flash('success').toString(),
-		error: req.flash('error').toString()
-	});
+  res.render('reg', {
+    title: 'Reg',
+    user: req.session.user,
+    success: req.flash('success').toString(),
+    error: req.flash('error').toString()
+  });
 });
 
 router.post('/reg', function(req, res) {
-	var name = req.body.Name;
-	var email = req.body.Email;
-	var password = req.body.Password;
-	var repassword = req.body.Repassword;
-	if (password != repassword) {
-		req.flash('error', '密码不一致');
-		return res.redirect('/reg');
-	}
-	var md5 = crypto.createHash('md5');
-	var password = md5.update(password).digest('hex');
-	var newUser = new User({
-		name: name,
-		password: password,
-		email: email
-	});
-	User.get(newUser.name, function(err, user) {
-		if (err) {
-			req.flash('error', err);
-			res.redirect('/reg');
-		}
-		if (user) {
-			req.flash('error', '用户已经存在');
-			res.redirect('/reg');
-		}
-		newUser.save(function(err, user) {
-			if (err) {
-				req.flash('error', err);
-				return res.redirect('/reg');
-			}
-			req.session.user = user;
-			req.flash('success', '注册成功');
-			res.redirect('/');
-		});
-	});
+  var name = req.body.Name;
+  var email = req.body.Email;
+  var password = req.body.Password;
+  var repassword = req.body.Repassword;
+  if (password != repassword) {
+    req.flash('error', '密码不一致');
+    return res.redirect('/reg');
+  }
+  var md5 = crypto.createHash('md5');
+  var password = md5.update(password).digest('hex');
+  var newUser = new User({
+    name: name,
+    password: password,
+    email: email
+  });
+  User.get(newUser.name, function(err, user) {
+    if (err) {
+      req.flash('error', err);
+      res.redirect('/reg');
+    }
+    if (user) {
+      req.flash('error', '用户已经存在');
+      res.redirect('/reg');
+    }
+    newUser.save(function(err, user) {
+      if (err) {
+        req.flash('error', err);
+        return res.redirect('/reg');
+      }
+      req.session.user = user;
+      req.flash('success', '注册成功');
+      res.redirect('/');
+    });
+  });
 });
 
 // Login
 router.get('/login', function(req, res, next) {
-	res.render('login', {
-		title: 'Login',
-		user: req.session.user,
-		success: req.flash('success').toString(),
-		error: req.flash('error').toString()
-	});
+  res.render('login', {
+    title: 'Login',
+    user: req.session.user,
+    success: req.flash('success').toString(),
+    error: req.flash('error').toString()
+  });
 });
 
 router.post('/login', function(req, res) {
-	var md5 = crypto.createHash('md5');
-	var password = md5.update(req.body.Password).digest('hex');
+  var md5 = crypto.createHash('md5');
+  var password = md5.update(req.body.Password).digest('hex');
 
-	User.get(req.body.Name, function(err, user) {
-		if (!user) {
-			req.flash('error', '用户不存在');
-			return res.redirect('/login');
-		}
-		if (password != user.password) {
-			req.flash('error', '密码错误');
-			return res.redirect('/login');
-		}
-		req.session.user = user;
-		req.flash('success', '登录成功');
-		res.redirect('/');
-	});
+  User.get(req.body.Name, function(err, user) {
+    if (!user) {
+      req.flash('error', '用户不存在');
+      return res.redirect('/login');
+    }
+    if (password != user.password) {
+      req.flash('error', '密码错误');
+      return res.redirect('/login');
+    }
+    req.session.user = user;
+    req.flash('success', '登录成功');
+    res.redirect('/');
+  });
 });
 
 // Logout
 router.get('/logout', function(req, res, next) {
-	req.session.user = null;
-	req.flash('success', '登出成功');
-	res.redirect('/');
+  req.session.user = null;
+  req.flash('success', '登出成功');
+  res.redirect('/');
 });
 
 // Post
 router.get('/post', function(req, res, next) {
-	res.render('post', {
-		title: 'Post',
-		user: req.session.user,
-		success: req.flash('success').toString(),
-		error: req.flash('error').toString()
-	});
+  res.render('post', {
+    title: 'Post',
+    user: req.session.user,
+    success: req.flash('success').toString(),
+    error: req.flash('error').toString()
+  });
 });
 
 router.post('/post', function(req, res) {
-	var currentUser = req.session.user;
-	var post = new Post(currentUser.name, req.body.title, req.body.post);
-	post.save(function(err) {
-		if (err) {
-			req.flash('error', err);
-			return res.redirect('/');
-		}
-		req.flash('success', '发表成功');
-		res.redirect('/');
-	});
+  var currentUser = req.session.user;
+  var post = new Post(currentUser.name, req.body.title, req.body.post);
+  post.save(function(err) {
+    if (err) {
+      req.flash('error', err);
+      return res.redirect('/');
+    }
+    req.flash('success', '发表成功');
+    res.redirect('/');
+  });
 });
 
 // Upload
 router.get('/upload', function(req, res) {
-	res.render('upload', {
-		title: 'Upload',
-		user: req.session.user,
-		success: req.flash('success').toString(),
-		error: req.flash('error').toString()
-	});
+  res.render('upload', {
+    title: 'Upload',
+    user: req.session.user,
+    success: req.flash('success').toString(),
+    error: req.flash('error').toString()
+  });
 });
 
 router.post('/upload', function(req, res, next) {
-	req.flash('success', 'Upload success!');
-	res.redirect('/upload');
+  req.flash('success', 'Upload success!');
+  res.redirect('/upload');
 });
 
 // Show
 router.get('/u/:name', function(req, res) {
-	User.get(req.params.name, function(err, user) {
-		if(!user) {
-			req.flash('error', '用户不存在');
-			return res.redirect('/');
-		}
-		Post.get(user.name, function(err, posts) {
-			if(err) {
-				req.flash('error', err);
-				return res.redirect('/');
-			}
-			res.render('user', {
-				title: user.name,
-				posts: posts,
-				user: req.session.user,
-				success: req.flash('success').toString(),
-				error: req.flash('error').toString()
-			});
-		});
-	});
+  User.get(req.params.name, function(err, user) {
+    if(!user) {
+      req.flash('error', '用户不存在');
+      return res.redirect('/');
+    }
+    Post.get(user.name, function(err, posts) {
+      if(err) {
+        req.flash('error', err);
+        return res.redirect('/');
+      }
+      res.render('user', {
+        title: user.name,
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
 });
 
 router.get('/u/:name/:day/:title', function(req, res) {
-	Post.getOne(req.params.name, req.params.day, req.params.title, function(err, post) {
-		if(err) {
-			req.flash('error', err);
-			return res.redirect('/');
-		}
-		res.render('article', {
-			title: req.params.name,
-			post: post,
-			user: req.session.user,
-			success: req.flash('success').toString(),
-			error: req.flash('error').toString()
-		});
-	});
+  Post.getOne(req.params.name, req.params.day, req.params.title, function(err, post) {
+    if(err) {
+      req.flash('error', err);
+      return res.redirect('/');
+    }
+    res.render('article', {
+      title: req.params.name,
+      post: post,
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
+  });
 });
 
 // Edit
 router.get('/edit/:name/:day/:title', function(req, res) {
-	var currentUser = req.session.user;
-	Post.edit(req.params.name, req.params.day, req.params.title, function(err, post) {
-		if(err) {
-			req.flash('error', err);
-			return res.redirect(back);
-		}
-		console.log(post);
-		res.render('edit', {
-			title: 'Edit',
-			post: post,
-			user: req.session.user,
-			success: req.flash('success').toString(),
-			error: req.flash('error').toString()
-		});
-	});
+  var currentUser = req.session.user;
+  Post.edit(req.params.name, req.params.day, req.params.title, function(err, post) {
+    if(err) {
+      req.flash('error', err);
+      return res.redirect(back);
+    }
+    console.log(post);
+    res.render('edit', {
+      title: 'Edit',
+      post: post,
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
+  });
 });
 
 module.exports = router;

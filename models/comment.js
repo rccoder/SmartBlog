a76728ce19c903c5358556d54553a1a4e701c1e2
rccoder <rccoder.net@gmail.com>
2015-04-28 16:23:53 +0800

    update the style

--- models/comment.js
+++ models/comment.js
@@ -1,41 +1,41 @@
 var mongodb = require('./db');
 
 function Comment(name, day, title, comment) {
-	this.name = name,
-	this.title = title,
-	this.day = day,
-	this.comment = comment
+  this.name = name,
+  this.title = title,
+  this.day = day,
+  this.comment = comment
 };
 
 module.exports = Comment;
 
 Comment.prototype.save = function(callback) {
-	var name = this.name;
-	var day = this.day;
-	var title = this.title;
-	var comment = this.comment;
+  var name = this.name;
+  var day = this.day;
+  var title = this.title;
+  var comment = this.comment;
 
-	mongodb.open(function(err, db) {
-		if(err) {
-			return callback(err);
-		}
-		db.collection('posts', function(err, collection) {
-			if(err) {
-				mongodb.close();
-				return callback(err);
-			}
-			collection.update({
-				"name": name,
-				"time.day": day,
-				"title": title
-			}, {$push: {"comments": comment}
-		}, function(err) {
-			mongodb.close();
-			if(err) {
-				return callback(err);
-			}
-			callback(null);
-		});
-		});
-	});
+  mongodb.open(function(err, db) {
+    if(err) {
+      return callback(err);
+    }
+    db.collection('posts', function(err, collection) {
+      if(err) {
+        mongodb.close();
+          return callback(err);
+    	}
+      collection.update({
+        "name": name,
+        "time.day": day,
+        "title": title
+      }, {$push: {"comments": comment}
+    }, function(err) {
+      mongodb.close();
+      if(err) {
+        return callback(err);
+      }
+      callback(null);
+    });
+  });
+});
 };
